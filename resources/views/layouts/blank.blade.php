be0d7155cf3fc91cc36563e0bdf9e7e2ea93b125
Elizabethdev <31627638+Elizabethdev@users.noreply.github.com>
2020-09-10 21:11:48 -0500

    vue
    install vue and create login
    config database

---
+++ resources/views/layouts/blank.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+	<link rel="stylesheet" href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+	<link rel="stylesheet" href="{{ asset('assets/css/sb-admin-2.min.css') }}">
+
+	@yield('css')
+
+</head>
+<<body class="bg-gradient-primary">
+
+<div class="container">
+
+  <!-- Outer Row -->
+  <div class="row justify-content-center">
+
+    <div class="col-xl-10 col-lg-12 col-md-9">
+
+      <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card-body p-0">
+          <!-- Nested Row within Card Body -->
+          <div class="row">
+            @yield('contenido')
+          </div>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+    <!-- Bootstrap core JavaScript-->
+	<script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+	<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+	<script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+
+	@yield('scripts')
+</body>
+
+
+</html>
