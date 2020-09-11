83c2ef9210ee00654dd3a1a5afef67c53e108629
Elizabethdev <31627638+Elizabethdev@users.noreply.github.com>
2020-09-11 17:11:24 -0500

    cambio img login
    pequeños cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,42 @@
 @extends('layouts.blank')
-
+@section('title')
+Login
+@stop
 @section('contenido')
 <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
     <div class="col-lg-6">
         <div class="p-5">
-        <div class="text-center">
-            <h1 class="h4 text-gray-900 mb-4">{{ __('Login') }}</h1>
-        </div>
-        <form class="user " method="POST" action="{{ route('login') }}">
-        @csrf
-            <div class="form-group">
-            <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="{{ __('E-Mail Address') }}">
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
+            <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">{{ __('Login') }}</h1>
             </div>
-            <div class="form-group">
-                <input id="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="{{ __('Password') }}">
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-            <div class="form-group">
-                <div class="custom-control custom-checkbox small">
-
-                    <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                    <label class="custom-control-label" for="remember">{{ __('Remember Me') }}</label>
+            <form class="user " method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group">
+                    <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="{{ __('E-Mail Address') }}">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                 </div>
-            </div>
-
-            <button type="submit" class="btn btn-primary btn-user btn-block">
-                {{ __('Login') }}
-            </button>
-
-        </form>
+                <div class="form-group">
+                    <input id="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="{{ __('Password') }}">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <div class="custom-control custom-checkbox small">
+                        <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="custom-control-label" for="remember">{{ __('Remember Me') }}</label>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-user btn-block">
+                    {{ __('Login') }}
+                </button>
+            </form>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/datos/uploadfile.blade.php
+++ resources/views/datos/uploadfile.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
-
+@section('title')
+Uploadfile
+@stop
 @section('contenido')
     <upload-component></upload-component>
 @endsection
