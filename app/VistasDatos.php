9dbc3da69a0a73eeaf6842d5960e8539821533e6
Elizabethdev <31627638+Elizabethdev@users.noreply.github.com>
2020-10-30 13:53:45 -0600

    cambios
    tabla alc

--- app/VistasDatos.php
+++ app/VistasDatos.php
@@ -119,7 +119,7 @@ class VistasDatos extends Model
     }
     public function getDatosTotalesALC_COB($where, $order)
     {
-        $total = DB::select('SELECT UPPER("") as cve_u, UPPER("") as cve_edo, UPPER("") as estado, UPPER("") as consejo_cuenca, UPPER("") as cve_mun, UPPER("") as municipio, 
+        $total = DB::select('SELECT UPPER(" ") as cve_u, UPPER("") as cve_edo, UPPER("") as estado, UPPER("") as consejo_cuenca, UPPER("") as cve_mun, UPPER("") as municipio, 
         UPPER("") as cve_subcuenca, UPPER("") as subcuenca, UPPER("") as reg_economica, UPPER("") as num_region, UPPER("") as localidad, UPPER("") as RANGO_PI, UPPER("") as POBTOT_20, 
         UPPER("") as PO_CON_ALC_15, UPPER("") as PO_CON_ALC_20, UPPER("") as PO_CON_ALC_30, UPPER("") as R_POB_15, UPPER("") as R_POB_20, UPPER("") as R_POB_30, UPPER("TOTAL") as TIPO_20, SUM(COB_ALC_10) as COB_ALC_10, SUM(COB_ALC_15) as COB_ALC_15, SUM(COB_ALC_20) as COB_ALC_20, 
         SUM(COB_ALC_30) as COB_ALC_30, UPPER("") as R_COB_ALC_15, UPPER("") as R_COB_ALC_20, UPPER("") as R_COB_ALC_30 FROM vw_cobertura_ALC '.$where);
