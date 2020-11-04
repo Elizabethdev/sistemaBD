106d0279957f7fb0da225ff93cb811ecb6b4fa85
Elizabethdev <31627638+Elizabethdev@users.noreply.github.com>
2020-11-04 00:37:47 -0600

    descargar excel ALC - pob
    descargar excel e imprimir ALC -pob

--- resources/js/pagealpob.js
+++ resources/js/pagealpob.js
@@ -1,9 +1,10 @@
 import filtrosComponent from './components/generales/filtros.vue';
-import tableComponent from './components/generales/tablealpob.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import tableComponent from './components/generales/tablealpob.vue';
+import btnComponent from './components/generales/btn.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import axios from './client/client.js';
-import { BOverlay} from 'bootstrap-vue'
+import { BOverlay} from 'bootstrap-vue';
 
 window.Vue = require('vue');
 
@@ -12,10 +13,12 @@ const app = new Vue({
     components: {
         filtrosComponent,
         tableComponent,
+        btnComponent,
         BOverlay
     },
     data: {
         show:false,
+        busy:false,
         headersTable: [
             {name:'Estado', visible: true},
             {name:'Consejo de Cuenca', visible: false},
@@ -34,8 +37,6 @@ const app = new Vue({
             {name:'Población Sin Alcantarillado 2030', visible: true}
         ],
         newdtotales: [],
-        newdmunicipios: {},
-        tipoVista: 'consejo',
         visible: {
             municipio: false,
             consejo: false,
@@ -53,6 +54,32 @@ const app = new Vue({
             rpoblacion: [],
             año: [],
             pi: [],
+        },
+        headersFile: {
+            cve_u:"cve_u",
+            cve_edo: 'cve_edo',
+            estado:"estado",
+            consejo_cuenca: 'consejo_cuenca',
+            cve_mun: 'cve_mun',
+            municipio:'municipio',
+            cve_subcuenca:'cve_subcuenca', 
+            subcuenca:'subcuenca', 
+            reg_economica:'reg_economica', 
+            num_region:'num_region', 
+            localidad:'localidad',
+            // POBTOT_10:'POBTOT_10', 
+            // POBTOT_15:'POBTOT_15', 
+            // POBTOT_30:'POBTOT_30', 
+            POBTOT_20:'POBTOT_20', 
+            TIPO_20:'TIPO_20', 
+            PO_CON_ALC_10:'PO_CON_ALC_10', 
+            PO_SIN_ALC_10:'PO_SIN_ALC_10',
+            PO_CON_ALC_15:'PO_CON_ALC_15',
+            PO_SIN_ALC_15:'PO_SIN_ALC_15', 
+            PO_CON_ALC_20:'PO_CON_ALC_20', 
+            PO_SIN_ALC_20:'PO_SIN_ALC_20', 
+            PO_CON_ALC_30:'PO_CON_ALC_30', 
+            PO_SIN_ALC_30:'PO_SIN_ALC_30', 
         }
     },
     methods: {
@@ -107,6 +134,43 @@ const app = new Vue({
                 console.log(error)
                 this.show = false
             })
+        },
+        guardarexcel(){
+            let data = [...this.newdtotales]
+            data.splice(0,0,this.headersFile)
+            this.busy = true
+            axios.post('/ap/export',{datos: data, page: 'poblacion'}
+            , {
+                responseType: 'blob'
+            })
+            .then((response)=>{
+                const url = URL.createObjectURL(new Blob([response.data], {
+                    type: 'application/vnd.ms-excel'
+                }))
+                const link = document.createElement('a')
+                link.href = url
+                link.setAttribute('download', 'pob_con_sin_alc.xlsx')
+                document.body.appendChild(link)
+                link.click()
+                this.busy = false
+            })
+            .catch(error => {
+                console.log(error)
+                this.busy = false
+            })
+        },
+        print(nombreDiv){
+            var w = window.open();
+            w.document.write('<html><head>');
+            w.document.write('<style>.tabla{width:100%;border-collapse:collapse;margin:16px 0 16px 0;}.tabla th{border:1px solid #ddd;padding:4px;background-color:#4c5c96;text-align:left;font-size:15px;color: #fff;}.tabla td{border:1px solid #ddd;text-align:left;padding:6px;}</style>');
+            w.document.write('</head><body>');
+            w.document.write(document.getElementById(nombreDiv).innerHTML);
+            w.document.write('</body></html>');
+            w.document.close(); // necesario para IE >= 10
+            w.focus(); // necesario para IE >= 10
+            w.print();
+            w.close();
+            return true;
         }
     }
 });
